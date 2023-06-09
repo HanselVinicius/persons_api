1d883aabfe0a15abb664146599b42eddb3dd757b
Vinicius Hansel <hanelvinicius@gmail.com>
2023-06-09 13:55:16 -0300

    chore: persons_models

--- src/models/persons_models.js
+++ src/models/persons_models.js
@@ -1,9 +1,39 @@
-import { DataType } from "sequelize";
+import { DataTypes } from "sequelize";
 import sequelize from "../data/sql/connection_config.js";
 
 
 const Person = sequelize.define(
     'persons_tb',{
-        
+        nome:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        idade: {
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        data_de_nascimento: {
+            type:DataTypes.DATE,
+            allowNull:false
+        },
+        id_endereco: {
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            references:{
+                model:'endereco_tb',
+                key: 'id'
+            }
+        }, 
+        cpf:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        isDeleted:{
+            type: DataTypes.BOOLEAN,
+            allowNull:false
+        }
+
+
+
     }
 )
